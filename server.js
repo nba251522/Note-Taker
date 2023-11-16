fdcedc5bbcc51595669fb9a6342984f1f83b3914
nba251522 <inquister112345678@gmail.com>
2023-11-16 02:30:06 -0800

    updated express.js and added a get route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,4 +11,14 @@ app.use(express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+});
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile(path.join(__dirname, './db/db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ message: "Error reading notes" });
+    }
+    res.json(JSON.parse(data));
+  });
 });
